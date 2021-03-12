a216789526d308a6fb0a727ec2bea38c0ac1e9a8
Marina <badbilliejoe@gmail.com>
2021-03-12 17:23:23 +0300

    add html and script file

---
+++ script.js
@@ -0,0 +1,19 @@
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres:[],
+    privat: false
+};
+
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('На сколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('На сколько оцените его?', '');
+
+personalMovieDB.movies[a] = b; //movies['ответ на первый вопрос'] = "ответ на второй вопрос"
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
